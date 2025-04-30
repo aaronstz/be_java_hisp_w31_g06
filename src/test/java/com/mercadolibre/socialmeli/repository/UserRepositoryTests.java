b645a42f65147bc1c2c1b2eef5057b88bebbc680
jbellavitis_meli <juan.bellavitis@mercadolibre.com>
2025-04-30 13:00:32 -0300

    Packages and files for testing added

---
+++ src/test/java/com/mercadolibre/socialmeli/repository/UserRepositoryTests.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.socialmeli.repository;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class UserRepositoryTests {
+
+    private UserRepositoryImpl repository;
+}
---
+++ src/test/java/com/mercadolibre/socialmeli/service/UserServiceTests.java
@@ -0,0 +1,19 @@
+package com.mercadolibre.socialmeli.service;
+
+import com.mercadolibre.socialmeli.repository.UserRepositoryImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTests {
+
+    @Mock
+    private UserRepositoryImpl repository;
+
+    @InjectMocks
+    private UserServiceImpl service;
+}
