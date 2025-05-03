9a361c85eaa52a0f69ac5f6d91641a8a903b65f2
fernando <fernandogabriel.torres@mercadolibre.com>
2025-05-03 13:03:40 -0300

    test unitario y de integracion solicitados en T0007

--- src/test/java/com/mercadolibre/socialmeli/BeJavaHispW31G06ApplicationTests.java
+++ src/test/java/com/mercadolibre/socialmeli/BeJavaHispW31G06ApplicationTests.java
@@ -1,8 +1,11 @@
 package com.mercadolibre.socialmeli;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.socialmeli.dto.FollowerCountDto;
 import com.mercadolibre.socialmeli.dto.PostDto;
 import com.mercadolibre.socialmeli.repository.ProductRepositoryImpl;
 import com.mercadolibre.socialmeli.util.TestDataFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,14 +14,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -55,5 +60,27 @@ class BeJavaHispW31G06ApplicationTests {
 		assertFalse(responseList.isEmpty());
 		assertTrue(responseList.stream().allMatch(p -> p.getHasPromo() && p.getDiscount() > 0));
 	}
+
+
+	@DisplayName("getFollowersCount should return the number of followers of the user when the input data is valid")
+	@Test
+	public void getFollowersCount_shouldReturnCorrectFollowerCount_whenInputIsValid() throws Exception {
+		MvcResult result = this.mockMvc.perform(get("/users/1/followers/count"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.followersCount").value(2))
+				.andReturn();
+
+		String responseBody = result.getResponse().getContentAsString();
+		assertNotNull(responseBody);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		FollowerCountDto responseDto = objectMapper.readValue(responseBody, FollowerCountDto.class);
+
+		assertTrue(responseDto.getFollowersCount() >= 0);
+		Assertions.assertEquals(2, responseDto.getFollowersCount());
+	}
+
 
 }
